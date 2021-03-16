6f44808a03e910c9f0914abd3a99bdf0297673c7
Elina <elina.zekunde@gmail.com>
2021-03-16 13:39:16 +0200

    - implement the "equals" button

--- Calculator/Models/Calculator.swift
+++ Calculator/Models/Calculator.swift
@@ -66,7 +66,49 @@ class Calculator: ObservableObject {
         decimalPlace = 0
     }
     
+    // Returns true if division by 0 could happen
+    func checkForDivision() -> Bool {
+        
+        if currentOperator!.isDivision && (currentNumber == nil && previousNumber == 0 || currentNumber == 0) {
+            displayValue = "Error"
+            reset()
+            return true
+        }
+        return false
+    }
+    
     func equalsClicked() {
+        
+        // Check if we have an operation to perform
+        if currentOperator != nil {
+            
+            // Reset the decimal place for the current number
+            decimalPlace = 0
+            
+            // Guard for division by 0
+            if checkForDivision() { return }
+            
+            // Check if we have at least one operand
+            if currentNumber != nil || previousNumber != nil {
+                
+                // Compute the total
+                let total = currentOperator!.op(previousNumber ?? currentNumber!, currentNumber ?? previousNumber!)
+                
+                // Update the first operand
+                if currentNumber == nil {
+                    currentNumber = previousNumber
+                }
+                
+                // Update the second operand
+                previousNumber = total
+                
+                // Set the "=" flag
+                equaled = true
+                
+                // Update the UI
+                setDisplayValue(number: total)
+            }
+        }
         
     }
     
@@ -121,6 +163,7 @@ class Calculator: ObservableObject {
         
         // If we have two operands, compute them
         if currentNumber != nil && previousNumber != nil {
+            if checkForDivision() { return }
             let total = currentOperator!.op(previousNumber!, currentNumber!)
             previousNumber = total
             currentNumber = nil
