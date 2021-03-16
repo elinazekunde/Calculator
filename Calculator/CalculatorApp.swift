a8423b8fba510dd576ad9d0539ac4e1f5123278d
Elina <elina.zekunde@gmail.com>
2021-03-16 11:47:05 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Elīna Zekunde on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
